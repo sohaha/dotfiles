1086ab749e4520b823bcb0d3e3ebd0b4f0296af1
Ryan <seekwe@gmail.com>
2024-09-20 17:34:44 +0800

    备份目录

--- bash.sh
+++ bash.sh
@@ -16,10 +16,10 @@ export FZF_DEFAULT_OPTS=" \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
-if [ -f "$HOME/.fzf.bash" ] then
+if [ -f "$HOME/.fzf.bash" ]; then
   if [ "$my_shell" = "zsh" ]; then
     source "$HOME/.fzf.bash"
   else
     source "$HOME/.fzf.zsh"
   fi
-end
+fi
--- setup.sh
+++ setup.sh
@@ -2,8 +2,27 @@
 
 # set -e
 
-# symlinks
+BACKUP_DIR=$HOME/.dotfiles_bak
+if [ ! -d "$BACKUP_DIR" ]; then
+  mkdir -p "$BACKUP_DIR"
+fi
+
+
 [ -d "$HOME/.config" ] && ln -s $HOME/dotfiles/.config/* $HOME/.config || ln -s $HOME/dotfiles/.config $HOME
-[ -f "$HOME/.tmux.conf" ] || ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+
+ln_tmux() {
+    local TMUX_CONF="$HOME/.tmux.conf"
+    TMUX_CONF="$HOME/.tmux.conf"
+    if [ -f "$TMUX_CONF" ]; then
+        backup_file="$BACKUP_DIR/.tmux.conf.backup.$(date +%Y%m%d%H%M%S)"
+        mv "$TMUX_CONF" "$backup_file"
+        echo "已将现有的 .tmux.conf 备份为 $backup_file"
+    fi
+
+    ln -s "$HOME/dotfiles/.tmux.conf" "$TMUX_CONF"
+    echo "已创建新的 .tmux.conf 软链接"
+}
+ln_tmux
+
 
 [ -d "$HOME/.fzf" ] || (git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf && $HOME/.fzf/install)
