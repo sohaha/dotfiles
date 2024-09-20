3cbdb55508c2b9705839e5d296d1b36f2664379d
Ryan <seekwe@gmail.com>
2024-09-20 16:49:20 +0800

    tmux 配置

--- bash.sh
+++ bash.sh
@@ -14,5 +14,3 @@ export FZF_DEFAULT_OPTS=" \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 [ -f "$HOME/.fzf.bash" ] && source "$HOME/.fzf.bash"
-bind "$(bind -s | command grep '^"\\ec"' | sed 's/ec/ed/')"
-[[ $- =~ i ]] && bind '"\ec": nop'
