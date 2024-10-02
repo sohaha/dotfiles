e78c3830db7c6a6d730b1fb5cd6812d9c298deb5
seekwe <seekwe@gmail.com>
2024-10-02 15:01:48 +0800

    追加 brew 软件

--- platform/macos/brew.sh
+++ platform/macos/brew.sh
@@ -2,15 +2,24 @@
 
 # 软件列表
 soft=(
-    "pearcleaner"    # 清理
-    "hammerspoon"    # 脚本控制
-    "keyboardholder" # 键盘映射
-    "monitorcontrol" # 显示器亮度控制
+  "pearcleaner"        # 清理
+  "hammerspoon"        # 脚本控制
+  "keyboardholder"     # 输入法切换
+  "monitorcontrol"     # 显示器亮度控制
+  "wezterm"            # 终端
+  "maccy"              # 剪切板管理
+  "neovim"             # NVIM 编辑器
+  "lulu"               # 防火墙
+  "battery"            # 电池
+  "karabiner-elements" # 键盘映射
+  "phoenix"            # 脚本控制
+  "responsively"       # 自适应开发环境
+  "snipaste"           # 截图
+  "utools"             # utools
+  "lazygit"            # GIT UI
 )
 
 # 安装软件
-for item in "${soft[@]}"
-do
-    brew install "$item"
+for item in "${soft[@]}"; do
+  brew install "$item"
 done
-
