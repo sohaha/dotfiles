538f7ed7b303bceebef349708c85fea86b5b587c
seekwe <seekwe@gmail.com>
2024-10-20 15:32:10 +0800

    优化初始化脚本

--- setup.sh
+++ setup.sh
@@ -8,21 +8,40 @@ if [ ! -d "$BACKUP_DIR" ]; then
 fi
 
 
-[ -d "$HOME/.config" ] && ln -s $HOME/dotfiles/.config/* $HOME/.config || ln -s $HOME/dotfiles/.config $HOME
-
-ln_tmux() {
-    local TMUX_CONF="$HOME/.tmux.conf"
-    TMUX_CONF="$HOME/.tmux.conf"
-    if [ -f "$TMUX_CONF" ]; then
-        backup_file="$BACKUP_DIR/.tmux.conf.backup.$(date +%Y%m%d%H%M%S)"
-        mv "$TMUX_CONF" "$backup_file"
-        echo "已将现有的 .tmux.conf 备份为 $backup_file"
-    fi
-
-    ln -s "$HOME/dotfiles/.tmux.conf" "$TMUX_CONF"
-    echo "已创建新的 .tmux.conf 软链接"
+link_file() {
+  local file=$1
+  local target=$2
+  local base_name=$(basename "$file")
+  if [ -e "$target" ]; then
+        if [ ! -L "$target" ]; then
+          backup_file="$BACKUP_DIR/${base_name}.backup.$(date +%Y%m%d%H%M%S)"
+          mv "$target" "$backup_file"
+          echo "已将现有的 $target 备份为 $backup_file"
+          ln -s "$file" "$target"
+          echo "$file -> $target"
+        else
+          echo "跳过 $target"
+        fi
+  else
+    ln -s "$file" "$target"
+    echo "$file -> $target"
+  fi
 }
-ln_tmux
+
+if [ -d "$HOME/.config" ]; then
+  if [ -d "$HOME/.config" ] && [ ! -L "$HOME/.config" ]; then
+    for file in $HOME/dotfiles/.config/*; do
+      base_name=$(basename "$file")
+      target="$HOME/.config/$base_name"
+      link_file "$file" "$target"
+    done
+  fi
+else
+  ln -s "$HOME/dotfiles/.config" "$HOME"
+  echo "已创建 $HOME/.config 的软链接"
+fi
+
+link_file "$HOME/dotfiles/.tmux.conf" "$HOME/.tmux.conf"
 
 
 [ -d "$HOME/.fzf" ] || (git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf && $HOME/.fzf/install)
