cf3d0cd737ed42259b02bcc31eaac857eb3b76b1
seekwe <seekwe@gmail.com>
2024-11-04 14:59:10 +0800

    更新 Yazi 配置

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -15,10 +15,48 @@ function Linemode:size_and_mtime()
 end
 
 require("git"):setup()
-require("starship"):setup()
 require("nvim"):setup()
 require("preview-visibility"):setup()
 require("full-border"):setup({
-  -- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
   type = ui.Border.ROUNDED,
 })
+
+require("projects"):setup({
+    save = {
+        method = "yazi", -- yazi | lua
+        lua_save_path = "", -- windows: "%APPDATA%/yazi/state/projects.json", unix: "~/.config/yazi/state/projects.json"
+    },
+    last = {
+        update_after_save = true,
+        update_after_load = true,
+    },
+    merge = {
+        quit_after_merge = false,
+    },
+    notify = {
+        enable = true,
+        title = "Projects",
+        timeout = 3,
+        level = "info",
+    },
+})
+
+function Status:name()
+  local h = self._tab.current.hovered
+  if not h then
+    return ui.Line {}
+  end
+
+  local linked = ""
+  if h.link_to ~= nil then
+    linked = " -> " .. tostring(h.link_to)
+  end
+  return ui.Line(" " .. h.name .. linked)
+end
+
+Header:children_add(function()
+  if ya.target_family() ~= "unix" then
+    return ui.Line {}
+  end
+  return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
