0cd1485c8b78e9f093b99e1582c67a9cbc90e0b0
seekwe <seekwe@gmail.com>
2024-11-11 18:23:41 +0800

    追加 网易云音乐

--- .config/wezterm/config/bindings.lua
+++ .config/wezterm/config/bindings.lua
@@ -33,8 +33,8 @@ local keys = {
 
   -- tabs --
   -- tabs: spawn+close
-  { key = "t", mods = mod.SUPER, action = act.SpawnTab("DefaultDomain") },
-  { key = "t", mods = mod.SUPER_REV, action = act.SpawnTab({ DomainName = "WSL:Ubuntu" }) },
+  { key = "t", mods = mod.SUPER_REV, action = act.SpawnTab("DefaultDomain") },
+  -- { key = "t", mods = mod.SUPER_REV, action = act.SpawnTab({ DomainName = "WSL:Ubuntu" }) },
   { key = "w", mods = mod.SUPER_REV, action = act.CloseCurrentTab({ confirm = false }) },
 
   -- tabs: navigation
--- platform/macos/brew.sh
+++ platform/macos/brew.sh
@@ -2,25 +2,26 @@
 
 # 软件列表
 soft=(
-  "pearcleaner"        # 清理
-  "hammerspoon"        # 脚本控制
-  "keyboardholder"     # 输入法切换
-  "monitorcontrol"     # 显示器亮度控制
-  "wezterm"            # 终端
-  "maccy"              # 剪切板管理
-  "neovim"             # NVIM 编辑器
-  "lulu"               # 防火墙
-  "battery"            # 电池
-  "karabiner-elements" # 键盘映射
-  "phoenix"            # 脚本控制
-  "responsively"       # 自适应开发环境
-  "snipaste"           # 截图
-  "utools"             # utools
-  "lazygit"            # GIT UI
-  "chsrc"              # 切换源
-  "mac-cleanup-py"     # 自动清理
-  "zen-browser"        # Zen Browser
-  "yazi"               # Yazi
+  "pearcleaner"                     # 清理
+  "hammerspoon"                     # 脚本控制
+  "keyboardholder"                  # 输入法切换
+  "monitorcontrol"                  # 显示器亮度控制
+  "wezterm"                         # 终端
+  "maccy"                           # 剪切板管理
+  "neovim"                          # NVIM 编辑器
+  "lulu"                            # 防火墙
+  "battery"                         # 电池
+  "karabiner-elements"              # 键盘映射
+  "phoenix"                         # 脚本控制
+  "responsively"                    # 自适应开发环境
+  "snipaste"                        # 截图
+  "utools"                          # utools
+  "lazygit"                         # GIT UI
+  "chsrc"                           # 切换源
+  "mac-cleanup-py"                  # 自动清理
+  "zen-browser"                     # Zen Browser
+  "yazi"                            # Yazi
+  "anhoder/go-musicfox/go-musicfox" # 网易云音乐
 )
 
 # 安装软件
