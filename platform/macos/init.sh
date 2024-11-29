d8bb9bad2792bcb6ec45e7f509baced297bd353c
seekwe <seekwe@gmail.com>
2024-11-29 17:49:10 +0800

    使用 F1-F12 键 作为功能键

--- platform/macos/init.sh
+++ platform/macos/init.sh
@@ -1,5 +1,7 @@
 chsh -s $(which zsh)
 
+defaults write -g com.apple.keyboard.fnState -bool true
+
 /bin/bash -c "$(curl -fsSL https://cdn.jsdelivr.net/gh/ineo6/homebrew-install/install)"
 
 eval "$(curl https://get.x-cmd.com)"
--- script/macos_setup.sh
+++ script/macos_setup.sh
@@ -5,6 +5,9 @@ link_file_hard "$HOME/dotfiles/config/vimo/VERSION" "$HOME/Library/Containers/Ch
 link_file_hard "$HOME/dotfiles/config/vimo/presets.db" "$HOME/Library/Containers/ChenghaoQ.Vimo-Rebinder/Data/Documents/Configs/presets.db"
 link_file_hard "$HOME/dotfiles/config/vimo/userdata.db" "$HOME/Library/Containers/ChenghaoQ.Vimo-Rebinder/Data/Documents/Configs/userdata.db"
 
+# 使用 F1-F12 键 作为功能键
+defaults write -g com.apple.keyboard.fnState -bool true
+
 source platform/macos/init.sh
 source platform/macos/x-app.sh
 source platform/macos/app.sh
