ab469911731321256b969bb8a4b8839eee956eb1
seekwe <seekwe@gmail.com>
2024-12-31 14:00:54 +0800

    更新按键

--- platform/others/x-app.sh
+++ platform/others/x-app.sh
@@ -3,10 +3,11 @@
 soft=(
   "bottom"     # 系统信息
   "s-tui"      # 系统信息
-  "hey"        # 压力测试
+  # "hey"        # 压力测试
   "nvim"       # 编辑器
   "ncdu"       # 磁盘管理
   "zoxide"     # 命令历史
+  # "oha"        # 负载测试
   # "trashy"   # 回收站
 )
 
