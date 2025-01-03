7dfe57b1d2eddc2cb30fea90ad94d34ac0ec071f
seekwe <seekwe@gmail.com>
2025-01-03 13:33:22 +0800

    add zsh

--- script/linux_setup.sh
+++ script/linux_setup.sh
@@ -2,6 +2,13 @@ sudo -v
 
 source platform/others/x-app.sh
 
+# 如果不存在 zsh
+if ! command -v zsh &> /dev/null; then
+   x install zsh
+fi
+
+chsh -s $(which zsh)
+
 # 如果不存在 chsrc 命令
 if ! command -v chsrc &> /dev/null; then
     curl https://chsrc.run/posix | sudo bash
