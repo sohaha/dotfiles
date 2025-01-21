d30563f6d0affac0bb279da88cb4ed2d633a4979
seekwe <seekwe@gmail.com>
2025-01-21 18:51:54 +0800

    添加 pay-respects

--- script/bash_alias.sh
+++ script/bash_alias.sh
@@ -1,3 +1,7 @@
 if command -v zoxide &> /dev/null; then
     eval "$(zoxide init bash)"
 fi
+
+if command -v pay-respects &> /dev/null; then
+    eval "$(pay-respects bash --alias)"
+fi
--- script/zsh_alias.sh
+++ script/zsh_alias.sh
@@ -1,3 +1,7 @@
 if command -v zoxide &> /dev/null; then
     eval "$(zoxide init zsh)"
 fi
+
+if command -v pay-respects &> /dev/null; then
+    eval "$(pay-respects zsh --alias)"
+fi
