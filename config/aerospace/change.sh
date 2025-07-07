2b2297e29fcb3646552735ebb0f96c2a0af2fa17
seekwe <seekwe@gmail.com>
2025-07-07 14:54:11 +0800

    更新 aerospace

--- config/aerospace/change.sh
+++ config/aerospace/change.sh
@@ -1,45 +1,19 @@
 #!/usr/bin/env sh
 
-ws=${1:-$AEROSPACE_FOCUSED_WORKSPACE}
+# current_workspace=$(aerospace list-workspaces --focused)
+# aerospace list-windows --all | grep -E "(Picture-in-Picture|Picture in Picture)" | awk '{print $1}' | while read window_id; do
+#     if [ -n "$window_id" ]; then
+#         aerospace move-node-to-workspace --window-id "$window_id" "$current_workspace"
+#     fi
+# done
 
-IFS=$'\n' all_wins=$(aerospace list-windows --all --format '%{window-id}|%{app-name}|%{window-title}|%{monitor-id}|%{workspace}')
-IFS=$'\n' all_ws=$(aerospace list-workspaces --all --format '%{workspace}|%{monitor-id}')
+set -e
+current_monitor=$(aerospace list-monitors --focused | awk '{print $1}')
+current_workspace=$(aerospace list-workspaces --focused)
+win_list=$(aerospace list-windows --monitor "$current_monitor" | grep -E "(Picture-in-Picture|Picture in Picture)" | awk '{print $1}')
 
-pip_titles=("Picture-in-picture" "Picture-in-Picture" "Picture in Picture" "Picture in picture" "微信" "WeChat" "画中画" "Simulator")
-
-find_pip_windows() {
-  local titles=("$@")
-  local result=""
-  for title in "${titles[@]}"; do
-    echo "$title"
-    local matches=$(printf '%s\n' "$all_wins" | grep "$title")
-    result="$result"$'\n'"$matches"
-  done
-  echo "$result" | sed '/^\s*$/d'
-}
-
-
-pip_wins=$(find_pip_windows "${pip_titles[@]}")
-target_mon=$(printf '%s\n' "$all_ws" | grep "^$ws" | cut -d'|' -f2 | xargs)
-
-move_win() {
-  local win="$1"
-
-  [[ -n $win ]] || return 0
-
-  local win_mon=$(echo "$win" | cut -d'|' -f4 | xargs)
-  local win_id=$(echo "$win" | cut -d'|' -f1 | xargs)
-  local win_app=$(echo "$win" | cut -d'|' -f2 | xargs)
-  local win_ws=$(echo "$win" | cut -d'|' -f5 | xargs)
-
-  # echo $target_mon -- "$win_mon"
-  # echo "$win_id" "$win_ws"
-  [[ $target_mon != "$win_mon" ]] && return 0
-  [[ $ws == "$win_ws" ]] && return 0
-
-  aerospace move-node-to-workspace --window-id "$win_id" "$ws"
-}
-
-echo "$pip_wins" | while IFS= read -r win; do
-  move_win "$win"
+echo "$win_list" | while IFS= read -r number; do
+  echo "Processing number: $number"
+  aerospace move-node-to-workspace --window-id "$number" "$current_workspace" < /dev/null
+  echo "continue"
 done
